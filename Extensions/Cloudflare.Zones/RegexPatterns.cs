6f27dd3ff8a01e0ab56e8b1d3101d4824357fcb0
Andreas Müller <webmaster@am-wd.de>
2025-06-24 10:48:40 +0200

    Reorganize Zone Extension

--- Extensions/Cloudflare.Zones/RegexPatterns.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.RegularExpressions;
-
-namespace AMWD.Net.Api.Cloudflare.Zones
-{
-	internal static class RegexPatterns
-	{
-		public static readonly Regex ZoneName = new(@"^([a-zA-Z0-9][\-a-zA-Z0-9]*\.)+[\-a-zA-Z0-9]{2,20}$", RegexOptions.Compiled);
-	}
-}
